68aeda28c62637649cefe0345efe3304ae115254
Sachin <skpkorba9009@gmail.com>
2022-12-06 00:40:10 +0530

    added elastic serach client.js file for the connection

--- src/elasticsearch/client.js
+++ src/elasticsearch/client.js
@@ -1,11 +1,11 @@
 const { Client } = require('@elastic/elasticsearch');
 
-const cofig = require('config');
+const config = require('config');
 
 const elasticConfig = config.get('elastic');
 
 const client = new Client({
-    could: {
+    cloud: {
         id: elasticConfig.cloudID,
     },
     auth: {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const client = require('./elasticsearch/client')
 const app = express();
 app.use((req,res) => {
  res.status(200).send("This is express Application") 
